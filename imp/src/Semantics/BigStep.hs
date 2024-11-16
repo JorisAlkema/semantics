6a96d6166d321917398bb7420c25ebbfc371eefe
Joris Alkema <jorisdcalkema@gmail.com>
2024-11-16 18:08:06 +0100

    imp

--- imp/src/Semantics/BigStep.hs
+++ imp/src/Semantics/BigStep.hs
@@ -8,9 +8,22 @@ import Semantics.Memory
 import Semantics.ExpEval
 
 evalCom :: Com -> Mem -> Mem
-evalCom x = case x of
-  CSkip -> failure x
-  CAssign ident aexp -> failure x
-  CSeq com1 com2 -> failure x
-  CIte bexp com1 com2 -> failure x
-  CWhile bexp com -> failure x
+evalCom x mem = case x of
+  CSkip -> mem
+  CAssign ident aexp -> 
+    let val = evalAExp aexp mem
+    in updateMem ident val mem
+  CSeq com1 com2 -> 
+    let mem' = evalCom com1 mem
+    in evalCom com2 mem'  
+  CIte bexp com1 com2 -> 
+    if evalBExp bexp mem 
+    then evalCom com1 mem
+    else evalCom com2 mem
+  CWhile bexp com -> 
+    if evalBExp bexp mem
+      then 
+      let mem' = evalCom com mem
+      in evalCom (CWhile bexp com) mem'
+    else mem --terminate
+
